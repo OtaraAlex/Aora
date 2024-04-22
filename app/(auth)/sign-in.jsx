c3f2a81b7fd179ac30eac128bad8519b42de5913
OtaraAlex <mbechealex61@gmail.com>
2024-04-22 12:09:14 +0300

    Add Profile screen and Signout functionality

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -6,9 +6,11 @@ import { Link, router } from "expo-router";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { signIn } from "../../lib/appwrite";
+import { getCurrentUser, signIn } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
   const [isSubmitting, setSubmitting] = useState(false);
   const [form, setForm] = useState({
     email: "",
@@ -25,7 +27,9 @@ const SignIn = () => {
     try {
       await signIn(form.email, form.password);
 
-      // set it to global state...
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLoggedIn(true);
 
       router.replace("/home");
     } catch (error) {
